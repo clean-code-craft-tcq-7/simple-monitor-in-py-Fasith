172ab857c25bcfa0993216d237f0332d06b91096
Fasith Ali K Mohamed (MS/EXV1-XC) <fak2kor@BAND-C-000XM.ban.apac.bosch.com>
2023-01-02 05:32:21 +0530

    refactor and add features

--- Test/test_check_limits.py
+++
@@ -1,6 +0,0 @@
-from check_limits import battery_is_ok
-
-
-def test_battery_is_okay():
-    assert (battery_is_ok(25, 70, 0.7) is True)
-    assert (battery_is_ok(50, 85, 0) is False)
--- check_limits_constants.py
+++
@@ -1,3 +0,0 @@
-ideal_temperature_range = (0, 45)
-ideal_state_of_charge_range = (20, 80)
-maximum_charge_rate = 0.8
